2c69dbae0534a550618be0fb9a49e1351bc960b3
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-15 20:12:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import '@fortawesome/fontawesome-free/css/all.css'
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todoList, setTodoList] = useState(["Lavar los platos", "Pasear al perro", "Hacer la cama", "Barrer"])
+
+  function addTodoValue(e) {
+    e.preventDefault()
+    let aux = todoList.concat(inputText);
+    setInputText("");
+    setTodoList([...aux]);
+  }
+
+  function deleteTodoValue(i){
+    let aux = todoList;
+    aux.splice(i,1);
+    setTodoList([...aux]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>todos</h1>
+      <div className="container">
+        <form onSubmit={(e) => addTodoValue(e)}>
+          <input type="text" placeholder="Ingrese una nueva tarea" onChange={e => setInputText(e.target.value)} value={inputText}></input>
+        </form>
+        { todoList !== null && todoList.length > 0? 
+        todoList.map((todo, i) => <div className="todo" key={i}> {todo} <i onClick={() => deleteTodoValue(i)} class="fas fa-times delete"></i></div> ) : ""}
+        <div className="items">{todoList.length === 1 ? todoList.length +" item left" : todoList.length +" items left"}</div>
+      </div>
     </div>
   );
 }
