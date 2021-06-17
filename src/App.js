5b7681ff7b2925bfa06f6c9970e7c82896a701eb
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-17 09:55:10 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '@fortawesome/fontawesome-free/css/all.css'
 import './App.css';
 
 function App() {
   const [inputText, setInputText] = useState("")
-  const [todoList, setTodoList] = useState(["Lavar los platos", "Pasear al perro", "Hacer la cama", "Barrer"])
+  const [todoList, setTodoList] = useState([])
 
-  function addTodoValue(e) {
-    e.preventDefault()
-    let aux = todoList.concat(inputText);
+
+  useEffect(() => {
+    getTodoList();
+  }, [])
+
+  const getTodoList = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/todos');
+      if (response.ok) {
+        const data = await response.json();
+        setTodoList(data);
+      }
+    } catch (error) {
+      console.log(error);
+
+    }
+  };
+
+
+  async function addTodoValue(e) {
+    e.preventDefault();
+    await fetch('http://localhost:3001/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ label: inputText, done: false }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
     setInputText("");
-    setTodoList([...aux]);
+    getTodoList();
   }
 
-  function deleteTodoValue(i){
-    let aux = todoList;
-    aux.splice(i,1);
-    setTodoList([...aux]);
+
+  async function deleteTodoValue(id) {
+    await fetch('http://localhost:3001/todos/'+ id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    getTodoList();
   }
+
+
+  // function addTodoValue(e) {
+  //   e.preventDefault();
+  //   let aux = todoList.concat({"label" : inputText, "done": false});
+  //   setInputText("");
+  //   setTodoList(aux);
+  // }
+
+
+  // function deleteTodoValue(i) {
+  //   let aux = todoList;
+  //   aux.splice(i, 1);
+  //   setTodoList([...aux]);
+  // }
 
   return (
     <div className="App">
@@ -26,9 +85,9 @@ function App() {
         <form onSubmit={(e) => addTodoValue(e)}>
           <input type="text" placeholder="Ingrese una nueva tarea" onChange={e => setInputText(e.target.value)} value={inputText}></input>
         </form>
-        { todoList !== null && todoList.length > 0? 
-        todoList.map((todo, i) => <div className="todo" key={i}> {todo} <i onClick={() => deleteTodoValue(i)} class="fas fa-times delete"></i></div> ) : ""}
-        <div className="items">{todoList.length === 1 ? todoList.length +" item left" : todoList.length +" items left"}</div>
+        {todoList !== null && todoList.length > 0 ?
+          todoList.map((todo, i) => <div className="todo" key={i}> {todo.label} <i onClick={() => deleteTodoValue(todo.id)} className="fas fa-times delete"></i></div>) : ""}
+        <div className="items">{todoList.length === 1 ? todoList.length + " item left" : todoList.length + " items left"}</div>
       </div>
     </div>
   );
